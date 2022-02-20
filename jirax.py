a947358c53f466aea232d6c65df3823adeb55a5d
xanterx <10103455+xanterx@users.noreply.github.com>
2022-02-20 23:55:17 +0530

    add pre-commit hook plugin file

--- jirax.py
+++ jirax.py
@@ -5,7 +5,7 @@ from typing import IO, Sequence
 
 
 def extract_jira(query: bytes) -> str | None:
-    jira_regex = re.compile("([A-Z][A-Z0-9]+-[0-9]+")
+    jira_regex = re.compile("([A-Z][A-Z0-9]+-[0-9]+)")
     jira_id = jira_regex.search(query.decode("utf-8"))
     if jira_id:
         return jira_id.group(1)
@@ -37,7 +37,7 @@ def check_and_update_commit_msg(file_obj: IO[bytes]) -> int:
         return 1
 
     file_obj.seek(0)
-    file_obj.write(commit_msg + f" [{jira_id}]".encode())
+    file_obj.write(commit_msg + f"\nJira: {jira_id}".encode())
     return 0
 
 
@@ -47,7 +47,7 @@ def main(argv: Sequence[str] | None = None) -> int:
     args = parser.parse_args(argv)
 
     retval = 1
-    filename = args.filename[0]
+    filename = args.filenames[0]
     with open(filename, "rb+") as file_obj:
         retval = check_and_update_commit_msg(file_obj)
     return retval
